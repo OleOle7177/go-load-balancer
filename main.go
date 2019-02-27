bdd8f4d7e5e61a154cf7b43ffe10fdcf19d99cab
OleOle7177 <ole_@bk.ru>
2019-02-27 18:19:51 +0300

    Add proxy logic

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
 	"sync"
 )
 
@@ -13,20 +10,10 @@ func main() {
 	sts := parseSettings("some path will be here")
 
 	for _, frontend := range sts.http {
-		mux := http.NewServeMux()
-		mux.HandleFunc(frontend.path, func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("Handled!")
-		})
-
-		listenTo := fmt.Sprintf(":%v", frontend.port)
-
+		frontend.launchServer()
 		wg.Add(1)
-		go func() {
-			if err := http.ListenAndServe(listenTo, mux); err != nil {
-				log.Fatal(err)
-			}
-		}()
 	}
+
 	wg.Wait()
 }
 
--- setup.go
+++ setup.go
@@ -1,12 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 type settings struct {
 	http []*httpProxy
 }
 
 type httpProxy struct {
-	port     int
-	path     string
+	port    int
+	servers map[string]*httpServer
+}
+
+type httpServer struct {
+	name     string
 	backends []*httpBackend
 }
 
@@ -15,25 +25,51 @@ type httpBackend struct {
 	proxyTo string
 }
 
+func (h *httpProxy) launchServer() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if val, ok := h.servers[r.Host]; ok {
+			fmt.Printf("should proxy to %s\n", val.name)
+
+		} else {
+			fmt.Println("No proxy for this host header")
+		}
+
+	})
+
+	listenTo := fmt.Sprintf(":%v", h.port)
+
+	go func() {
+		if err := http.ListenAndServe(listenTo, mux); err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
+
 func parseSettings(path string) *settings {
 	return &settings{http: []*httpProxy{
 		&httpProxy{
-			port:     5000,
-			path:     "/",
-			backends: []*httpBackend{&httpBackend{weight: 1, proxyTo: "http://sports.ru"}},
-		},
+			port: 5000,
+			servers: map[string]*httpServer{
+				"sports.ru": &httpServer{
+					name:     "sports",
+					backends: []*httpBackend{&httpBackend{weight: 1, proxyTo: "http://sports.ru"}},
+				},
+				"vc.ru": &httpServer{
+					name:     "vc",
+					backends: []*httpBackend{&httpBackend{weight: 1, proxyTo: "http://vc.ru"}},
+				},
+			}},
 		&httpProxy{
-			port:     5005,
-			path:     "/s",
-			backends: []*httpBackend{&httpBackend{weight: 1, proxyTo: "http://sports.ru"}},
+			port: 5005,
+			servers: map[string]*httpServer{
+				"yandex.ru": &httpServer{
+					name:     "yandex",
+					backends: []*httpBackend{&httpBackend{weight: 1, proxyTo: "http://yandex.ru"}},
+				},
+			},
 		},
 	},
 	}
 }
-
-// http:
-//  - port:80
-//    host:"some_host"
-//    backends:
-//      - weight: 20
-//        proxyTo: "http://sports.ru"
